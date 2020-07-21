de4b92b9ae0e35a7a1e689d4275eaf9723071fbf
leiyu13 <leiyu13@meituan.com>
2020-07-21 15:31:45 +0800

    添加README，修改部分逻辑，删除HTTPS

---
+++ README.md
@@ -0,0 +1,15 @@
+# Web Socket Demo
+
+A simple Web Socket Demo program in ASP.NET Core.
+
+## requirement
+
+.NET Core SDK 3.1
+
+## run
+
+```sh
+dotnet run
+```
+
+And then visit <http://localhost:5000> to see the demo.
--- SocketMiddleware.cs
+++ SocketMiddleware.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.WebSockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -34,12 +35,10 @@ namespace WebSocketDemo
                     {
                         try
                         {
-                            byte[] buffer = System.Text.Encoding.UTF8.GetBytes("Message from consumer");
                             for (var i = 0; i < 10; i++)
                             {
                                 Thread.Sleep(1000);
-                                await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
-                                Logger.LogInformation("Consumer Send Message");
+                                await Send(socket, "Message from server");
                             }
                         }
                         catch (Exception e)
@@ -50,12 +49,15 @@ namespace WebSocketDemo
                     consumer.Start();
                     while (!socket.CloseStatus.HasValue)
                     {
-                        var buffer = new byte[1024 * 4];
-                        WebSocketReceiveResult result = await Recieve(socket, buffer);
+                        var received = await Receive(socket);
+                        WebSocketReceiveResult result = received.result;
+                        string text = received.text;
                         while (!result.CloseStatus.HasValue)
                         {
-                            await Send(socket, buffer, result);
-                            result = await Recieve(socket, buffer);
+                            await Send(socket, "Server echo back: " +  text);
+                            received = await Receive(socket);
+                            result = received.result;
+                            text = received.text;
                         }
                         await socket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
                         Logger.LogCritical("Connection closed");
@@ -71,18 +73,21 @@ namespace WebSocketDemo
                 Logger.LogWarning("Exception: {0}", e?.Message ?? "no further infomation");
             }
         }
-
-        private async Task<WebSocketReceiveResult> Recieve(WebSocket socket, byte[] buffer)
+        private async Task<dynamic> Receive(WebSocket socket)
         {
+            byte[] buffer = new byte[1 << 12];
             var result = await socket.ReceiveAsync(buffer, CancellationToken.None);
-            Logger.LogInformation($"Recieved: {System.Text.Encoding.UTF8.GetString(buffer)}");
-            return result;
+            var text = Encoding.UTF8.GetString(buffer);
+            Logger.LogInformation($"Recieved: {text}");
+            return new { result, text };
         }
 
-        private async Task Send(WebSocket socket, byte[] buffer, WebSocketReceiveResult result)
+        private async Task Send(WebSocket socket, string message)
         {
-            await socket.SendAsync(new ArraySegment<byte>(buffer, 0, result.Count), result.MessageType, result.EndOfMessage, CancellationToken.None);
-            Logger.LogInformation($"Sended: {System.Text.Encoding.UTF8.GetString(buffer)}");
+            byte[] buffer = new byte[1 << 12];
+            var bytes = Encoding.UTF8.GetBytes(message);
+            await socket.SendAsync(bytes, WebSocketMessageType.Text, true, CancellationToken.None);
+            Logger.LogInformation($"Sended: {message}");
         }
     }
 
